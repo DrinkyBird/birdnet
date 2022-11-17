2a3696ac40899b3db0629db03ebcfc3fc43699ce
Sean Baggaley <me@drinkybird.net>
2022-11-17 21:47:00 +0000

    Remove another leftover script

--- scripts/make_maps.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/sean/website-subdomains/elite/maps
-/home/sean/website-subdomains/elite/scripts/galmap-build/bin/galmap -c /home/sean/website-subdomains/elite/scripts/scrape_config.yaml -r /home/sean/website-subdomains/elite/scripts/galmap/resources $*
